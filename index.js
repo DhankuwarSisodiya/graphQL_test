81f1cbda7edb213d48d4f4e0c8193152dc197b48
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-02-19 00:02:41 -0500

    Mutation types

--- index.js
+++ index.js
@@ -3,22 +3,35 @@ import schema from './schema';
 import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
+
 app.get('/', (req,res) => {
     res.send('graphQL is amazing');
 })
 
+const friendDatabase = {};
+class Friend {
+    constructor(id, {firstName, lastName, gender, email}) {
+        this.id = id,
+        this.firstName = firstName,
+        this.lastName = lastName,
+        this.gender = gender,
+        this.email = email
+    }
+}
 const root = { friend: () => {
     return {
         "id" : 123456789,
         "firstName": "Shefali",
         "lastName": "Vaidya",
         "gender": "Female",
-        "email": [
-            {email: "shef.vaid@gmail.com"},
-            {email: "shef.vaid95@gmail.com"}
-        ]
+        "email": "shef.vaid@gmail.com"
+    }},
+    createFriend: ({input}) => {
+        let id = require('crypto').randomBytes(10).toString('hex');
+        friendDatabase[id] = input;
+        return new Friend(id, input);
     }
-}}
+}
 app.use('/graphql', graphqlHTTP ({
     schema: schema,
     rootValue: root,
