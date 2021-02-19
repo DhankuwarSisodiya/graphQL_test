c675f069bf9fbae4c6d432d0441ff05e509ce359
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-02-18 23:16:11 -0500

    Initial GraphQL setup

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import schema from './schema';
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
 app.get('/', (req,res) => {
     res.send('graphQL is amazing');
 })
 
+const root = { hello: () => 'hii this is Dhan' }
+app.use('/graphql', graphqlHTTP ({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
 app.listen(8080, () => console.log("Running on server port localhost:8080/graphql"));
