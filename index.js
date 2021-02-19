5a95f46cfd53858b7b32159daf15f9aa7007da8f
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-02-18 23:00:48 -0500

    Server setup with latest ECMAScript

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+app.get('/', (req,res) => {
+    res.send('graphQL is amazing');
+})
+
+app.listen(8080, () => console.log("Running on server port localhost:8080/graphql"));
