cb08b3b1909407de0e2db171eb6ecdcd4f1fd55d
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-02-21 20:47:51 -0500

    Installing and using graphql tools

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import express from 'express';
-import schema from './schema';
 import { graphqlHTTP } from 'express-graphql';
-import resolvers from './resolvers';
+import {schema} from './schema';
 const app = express();
 
 app.get('/', (req,res) => {
     res.send('graphQL is amazing');
 })
 
-const root = resolvers; 
 
 app.use('/graphql', graphqlHTTP ({
     schema: schema,
-    rootValue: root,
     graphiql: true
 }))
 app.listen(8080, () => console.log("Running on server port localhost:8080/graphql"));
