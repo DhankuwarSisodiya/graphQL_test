324331fbfe4698780ec2b63603d072d95fbecfab
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-02-25 23:50:39 -0500

    Delete ites with mutations

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -3,50 +3,60 @@ import Sequelize from 'sequelize';
 import _ from 'lodash';
 import casual from 'casual';
 
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/friends',{
+// Mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/friends', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 const friendSchema = new mongoose.Schema({
-    firstName:{
+    firstName: {
         type: String
     },
-    lastName:{
+    lastName: {
         type: String
     },
-    gender:{
+    gender: {
         type: String
     },
-    age:{
+    age: {
         type: Number
     },
-    email:{
+    language: {
         type: String
     },
-    contacts:{
-        type: Array
+    email: {
+        type: String
     },
-})
+    contacts: {
+        type: Array
+    }
+});
 
-const Friends = mongoose.model('friends',friendSchema);
+const Friends = mongoose.model('friends', friendSchema);
 
+// SQL
 const sequelize = new Sequelize('database', null, null, {
     dialect: 'sqlite',
     storage: './alien.sqlite',
-
-})
-
-const Aliens = sequelize.define('aliens',{
-    firstName: {
-        type: Sequelize.STRING
-    },
-    lastName: {
-        type: Sequelize.STRING
-    },
-    planetName: {
-        type: Sequelize.STRING
-    },
 });
-export { Friends, Aliens }
+
+const Aliens = sequelize.define('aliens', {
+    firstName: { type: Sequelize.STRING },
+    lastName: { type: Sequelize.STRING },
+    planet: { type: Sequelize.STRING },
+});
+
+Aliens.sync({ force: true }).then(() => {
+    _.times(10, (i) => {
+        Aliens.create({
+            firstName: casual.first_name,
+            lastName: casual.last_name,
+            planet: casual.word,
+        });
+    });
+});
+
+
+export { Friends, Aliens };
